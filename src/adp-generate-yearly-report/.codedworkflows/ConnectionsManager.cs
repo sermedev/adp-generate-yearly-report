70cc9aae48fedfc907acf9339e10ca43cccf4223
sermedev <sermedev@outlook.com>
2025-06-28 16:59:26 +0200

    feat: Add logic to open and log in to ACME

---
+++ src/adp-generate-yearly-report/.codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace adpgenerateyearlyreport
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
